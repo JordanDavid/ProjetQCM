2d804370d15ebee9602dc1e03164c8411916402e
jordan <d1408davidj@21-UC36-02.ad.campus-eni.fr>
2015-10-20 15:50:44 +0200

    CSS Accueil, Menu.  V1

--- WebContent/js/fonction.js
+++ WebContent/js/fonction.js
@@ -1,3 +1,13 @@
 /**
- * 
+ * Contient l'ensemble des fonctions javascripts utilisées
  */
+
+/**
+ * Gestion de l'affichage du menu
+ */
+SelectionMenu = function(li){
+	//On enleve l'ancien li active
+	$(".liMenu.active").removeClass("active");
+	//ON place le nouveau li active
+	$(li).addClass("active");	
+}
