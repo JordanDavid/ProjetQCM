01bf2354e3fb1ddb65723631b520713932e50848
jordan <d1408davidj@21-UC36-02.ad.campus-eni.fr>
2015-10-20 10:07:51 +0200

    Création DALInscription et servlet authentification

---
+++ src/fr/eni_ecole/qcm/dal/DALInscription.java
@@ -0,0 +1,14 @@
+/**
+ * 20 oct. 2015
+ * QCM V1.0
+ * fr.eni_ecole.qcm.dal
+ */
+package fr.eni_ecole.qcm.dal;
+
+/**
+ * @author d1408davidj
+ * 20 oct. 2015
+ */
+public class DALInscription {
+
+}
---
+++ src/fr/eni_ecole/qcm/servlet/Authentification.java
@@ -0,0 +1,64 @@
+package fr.eni_ecole.qcm.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Authentification
+ */
+public class Authentification extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Authentification() {
+        super();
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		super.service(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request,response);
+	}
+
+	private void processRequest(HttpServletRequest request,
+			HttpServletResponse response) {
+		
+		
+	}
+
+}
