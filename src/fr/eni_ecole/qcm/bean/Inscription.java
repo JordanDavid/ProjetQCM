c7ed0149ef48e10415e9164c115d0022b13e990f
jordan <d1408davidj@21-UC36-02.ad.campus-eni.fr>
2015-10-20 09:55:43 +0200

    Création classe Inscription.java

---
+++ src/fr/eni_ecole/qcm/bean/Inscription.java
@@ -0,0 +1,14 @@
+/**
+ * 20 oct. 2015
+ * QCM V1.0
+ * fr.eni_ecole.qcm.bean
+ */
+package fr.eni_ecole.qcm.bean;
+
+/**
+ * @author d1408davidj
+ * 20 oct. 2015
+ */
+public class Inscription {
+
+}
