c208285ece9cdaee24cd67857d277275b2352dd9
jordan <d1408davidj@21-UC36-02.ad.campus-eni.fr>
2015-10-21 11:43:10 +0200

    Gestion référentiel V2 Import bibliothèque Jquery Datatable

--- WebContent/formateur/gestionReferentiel.jsp
+++ WebContent/formateur/gestionReferentiel.jsp
@@ -1,14 +1,86 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"
-    import ="fr.eni_ecole.qcm.bean.*,java.util.*" 
-   %>
-<% 
+	pageEncoding="UTF-8" import="fr.eni_ecole.qcm.bean.*,java.util.*"%>
+<%
 	String titre = "Gestion du référentiel";
 %>
-<%@include file="/fragments/haut.jspf" %>
-	<select id="themes" name="theme">
-		<% for(Theme theme : (List<Theme>)request.getAttribute("themes") ) { %>
-			<option><%=theme.getLibelle()%></option>
-		<% } %>
-	</select>
+<%@include file="/fragments/haut.jspf"%>
+<form id="gestion_referentiel"
+	action="<%=request.getContextPath()%>/formation/referentiel"
+	method="post">
+	<div id="div_theme">
+		<div id="div_select_theme">
+			<select id="themes" name="theme">
+				<option></option>
+			</select>
+		</div>
+		<div id="div_gestion_theme">
+			<input type="button" name="ajouterTheme" id="ajouterTheme"
+				value="Ajouter un thème" onclick="AfficherAjoutTheme();"> <input
+				type="submit" name="supprimerTheme" id="supprimerTheme"
+				value="Supprimer le thème" />
+		</div>
+	</div>
+	<div id="div_questions_theme">
+		<fieldset>
+			<legend>Liste des questions</legend>
+			<table id="lst_questions">
+				<tbody></tbody>
+			</table>
+		</fieldset>
+		<div id="div_btn_question">
+			<input type="button" name="ajouterQuestion" id="ajouterQuestion"
+				value="Ajouter une question" onclick="AfficherAjouterQuestion();" />
+			<input type="submit" name="supprimerQuestion" id="supprimerQuestion"
+				value="Supprimer la question" /> <input type="button"
+				name="changerTheme" id="changerTheme"
+				value="Changer le thème de la question">
+		</div>
+	</div>
+	<div id="div_details_question">
+		<fieldset>
+			<legend>Détail de la question</legend>
+			<div class="inline_div_reponse" align="left">
+				<label for="enonce">Énoncé :</label>
+			</div>
+			<div class="inline_div_reponse" align="left">
+				<textarea id="enonce" name="enonce" ></textarea>
+			</div>
+			<div class="inline_div_reponse" align="left">
+				<label for="image">Joindre une image</label> <input type="file"
+					name="image" id="image" />
+			</div>
+			<div class="inline_div_reponse" align="left">
+				<label for="typeQuestion">Type de la question :</label>
+			</div>
+			<div class="inline_div_reponse" align="left">
+				<select id="typeQuestion" name="typeQuestion" onchange="SelectionTypeQuestion();">
+					<option value="0">1 seule bonne réponse</option>
+					<option value="1">Plusieurs bonnes réponses</option>
+				</select>
+			</div>
+			<div class="inline_div_reponse" align="left">
+				<label>Ajouter les différentes réponses possibles :</label>
+				<div id="div_reponses_question">
+					<div class="reponse">
+						<input type="text" name="reponses[]" id="reponse_n" placeholder="Veuillez saisir la réponse"/>
+						<input type="radio" name="" id="" title="Cocher pour indiquer la bonne réponse"/>					
+					</div>		
+					<div class="reponse">
+						<input type="text" name="reponses[]" id="reponse_n" placeholder="Veuillez saisir la réponse"/>
+						<input type="radio" name="" id="" title="Cocher pour indiquer la bonne réponse"/>					
+					</div>					
+				</div>
+			</div>
+			<div class="inline_div_reponse" align="right">
+				<p class="blueLink underlineLink" onclick="ajouterReponse();">Ajouter
+					une reponse</p>
+			</div>
+			<div class="inline_div_reponse" align="center">
+				<input type="submit" name="enregisterQuestion"
+					id="enregistrerQuestion" value="Enregistrer" /> <input
+					type="button" name="annulerQuestion" id="annuler" value="Annuler" />
+			</div>
+		</fieldset>
+	</div>
+</form>
 <%@include file="/fragments/bas.jspf"%>
--- src/fr/eni_ecole/qcm/dal/DALTheme.java
+++ src/fr/eni_ecole/qcm/dal/DALTheme.java
@@ -5,10 +5,12 @@
  */
 package fr.eni_ecole.qcm.dal;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import fr.eni_ecole.qcm.bean.Theme;
@@ -29,7 +31,7 @@ public class DALTheme {
 	public static List<Theme> selectAll() throws SQLException {
 		Connection cnx= null;
 		Statement cmd = null;
-		List<Theme> ret = null;
+		List<Theme> ret = new ArrayList<Theme>();
 	
 		String sql = "SELECT * FROM THEME";
 		
@@ -39,9 +41,9 @@ public class DALTheme {
 			ResultSet res = cmd.executeQuery(sql);
 			
 			while(res.next()){
-				
-			}
-			
+				Theme theme = new Theme(res.getInt("idTheme"),res.getString("libelle_theme"));
+				ret.add(theme);
+			}			
 		}finally{
 			if(cmd != null)cmd.close();
 			if(cnx != null)cnx.close();
@@ -49,4 +51,120 @@ public class DALTheme {
 		return ret;
 	}
 
+	/**
+	 * Méthode en charge de récupérer le thème en fonction d'un identifiant 
+	 * 21 oct. 2015
+	 * @param theme Thème avec l'identifiant recherché
+	 * @return Le thème recherché
+	 * @throws SQLException
+	 */
+	public static Theme selectById(Theme theme) throws SQLException{
+		Connection cnx= null;
+		PreparedStatement cmd = null;
+	
+		String sql = "SELECT * FROM THEME WHERE THEME.idTheme = ?";
+		
+		try{
+			cnx = AccesBase.getConnection();
+			cmd = cnx.prepareStatement(sql);
+			cmd.setInt(1, theme.getIdTheme());
+			ResultSet res = cmd.executeQuery();
+			
+			if(res.next()){
+				theme.setLibelle(res.getString("libelle_theme"));
+			}else{
+				theme = null;
+			}			
+		}finally{
+			if(cmd != null)cmd.close();
+			if(cnx != null)cnx.close();
+		}
+		return theme;
+	}
+	
+	/**
+	 * Méthode en charge d'ajouter un thème à la BD 
+	 * 21 oct. 2015
+	 * @param theme Thème à ajouter
+	 * @return Le thème avec son id
+	 * @throws SQLException
+	 */
+	public static Theme ajouter(Theme theme) throws SQLException{
+		Connection cnx = null;
+		Statement st = null;
+		PreparedStatement cmd = null;
+		String sql = "INSERT INTO THEME(libelle_theme) VALUES (?)";
+		
+		try{
+			cnx = AccesBase.getConnection();
+			
+			cnx.setAutoCommit(false);
+			
+			cmd = cnx.prepareStatement(sql);
+			cmd.setString(1, theme.getLibelle());
+			cmd.executeUpdate();
+			
+			st = cnx.createStatement();
+			ResultSet rs = st.executeQuery("SELECT MAX(id)as New_Id FROM THEME");
+			if(rs.next()){
+				theme.setIdTheme(rs.getInt("New_Id"));
+			}
+			
+			cnx.commit();
+			
+		}catch(SQLException sqle){
+			if(cnx != null)
+				cnx.rollback();
+			throw sqle;
+		}finally{
+			if(st != null) st.close();
+			if(cmd != null)cmd.close();
+			if(cnx!=null)cnx.close();
+		}
+		return theme;
+	}
+
+	/**
+	 * Méthode en charge de modifier un thème 
+	 * 21 oct. 2015
+	 * @param theme Thème à modifier
+	 * @throws SQLException
+	 */
+	public static void modifier(Theme theme) throws SQLException{
+		Connection cnx = null;
+		PreparedStatement cmd = null;
+		String sql = "UPDATE THEME SET libelle_theme=? WHERE idTheme=?";
+		
+		try{
+			cnx = AccesBase.getConnection();
+			cmd = cnx.prepareStatement(sql);
+			cmd.setString(1, theme.getLibelle());
+			cmd.setInt(2, theme.getIdTheme());
+			cmd.executeUpdate();			
+		}finally{
+			if(cnx!=null)cnx.close();
+			if(cmd!=null)cmd.close();
+		}
+	}
+
+	/**
+	 * Méthode en charge de supprimer un thème 
+	 * 21 oct. 2015
+	 * @param theme
+	 * @throws SQLException
+	 */
+	public static void supprimer(Theme theme) throws SQLException{
+		Connection cnx = null;
+		PreparedStatement cmd = null;
+		String sql = "DELETE FROM THEME WHERE idTheme=?";
+		try{
+			cnx = AccesBase.getConnection();
+			cmd = cnx.prepareStatement(sql);
+			cmd.setInt(1, theme.getIdTheme());
+			cmd.executeUpdate();
+		}finally{
+			if(cmd!=null)cmd.close();
+			if(cnx!=null)cnx.close();
+		}
+	}
 }
